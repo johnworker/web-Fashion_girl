abe5e5572906551f80720a04665960a25e07ef45
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-17 15:35:39 +0800

    修改內容

--- main.js
+++ main.js
@@ -4,32 +4,34 @@ $("#banner").slick({
     autoplaySpeed: 1500
 });
 
-$("#work").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    // 響應式 RWD
-    responsive: [
-        // 平板：小於 1025
-        {
-            breakpoint: 1025,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        // 手機：小於 767
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+// 輪播小卡
+// $("#work").slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     // 響應式 RWD
+//     responsive: [
+//         // 平板：小於 1025
+//         {
+//             breakpoint: 1025,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         // 手機：小於 767
+//         {
+//             breakpoint: 767,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//     ]
+// });
 
+// about 輪播區 御用
 // $(".mini_box").slick({
 //     slidesToShow: 4,
 //     slidesToScroll: 1,
